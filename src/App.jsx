b47edb0625ef04c18fa0a7fa74f691b1272d89d8
matiascamba <matiezequielcamba@gmail.com>
2024-04-11 18:42:39 -0300

    componente detail del producto

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Home } from "./components/Home.jsx";
 import { Routes, Route } from "react-router-dom";
 import { Product } from "./components/Product.jsx";
+import ProductDetail from "./components/ProductDetail/ProductDetail.jsx";
 
 //Commit de prueba
 export const App = () => {
@@ -8,6 +9,7 @@ export const App = () => {
     <Routes>
       <Route path="/" element={<Home />}></Route>
       <Route path="/product" element={<Product />}></Route>
+      <Route path="/product/:id" element={<ProductDetail/>}></Route>
     </Routes>
   );
 };
