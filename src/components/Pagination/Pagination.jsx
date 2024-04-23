535f346c2462e6a1511c79e5a6ec1aef8413db9e
David Valdez Gramajo <davidpivito@gmail.com>
2024-04-23 09:49:18 -0300

    funcion resetScrollTop en componente de paginado

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -1,13 +1,23 @@
 const Pagination = ({ currentPage, totalPages, setPage }) => {
     const pages = Array.from({ length: totalPages }, (_, index) => index + 1);
 
+    const resetScrollTop = ()=>{
+        window.scrollTo(0, 0);
+    }
+
+    const handleClick = (page)=>{
+        setPage(page)
+        resetScrollTop()
+    }
+
     return (
-        <div className="flex gap-2 pb-8 justify-center">
+        <div className="flex ga
+        p-2 pb-8 justify-center">
             {pages.map((page) => (
                 <button
                     className="btn btn-outline"
                     key={page}
-                    onClick={() => setPage(page)}
+                    onClick={() => handleClick(page)}
                     disabled={currentPage === page}
                 >
                     {page}
