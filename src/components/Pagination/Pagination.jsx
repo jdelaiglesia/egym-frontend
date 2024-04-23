5b23a7078205f930576d81cb0ef4a18d31301085
RDG-Sanchez <rafaelzhx@gmail.com>
2024-04-23 16:01:55 -0300

    HOT FIX GAP PAGINATION

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -1,30 +1,29 @@
 const Pagination = ({ currentPage, totalPages, setPage }) => {
-    const pages = Array.from({ length: totalPages }, (_, index) => index + 1);
+  const pages = Array.from({ length: totalPages }, (_, index) => index + 1);
 
-    const resetScrollTop = ()=>{
-        window.scrollTo(0, 0);
-    }
+  const resetScrollTop = () => {
+    window.scrollTo(0, 0);
+  };
 
-    const handleClick = (page)=>{
-        setPage(page)
-        resetScrollTop()
-    }
+  const handleClick = (page) => {
+    setPage(page);
+    resetScrollTop();
+  };
 
-    return (
-        <div className="flex ga
-        p-2 pb-8 justify-center">
-            {pages.map((page) => (
-                <button
-                    className="btn btn-outline"
-                    key={page}
-                    onClick={() => handleClick(page)}
-                    disabled={currentPage === page}
-                >
-                    {page}
-                </button>
-            ))}
-        </div>
-    );
+  return (
+    <div className="flex gap-2 pb-8 justify-center">
+      {pages.map((page) => (
+        <button
+          className="btn btn-outline"
+          key={page}
+          onClick={() => handleClick(page)}
+          disabled={currentPage === page}
+        >
+          {page}
+        </button>
+      ))}
+    </div>
+  );
 };
 
 export default Pagination;
