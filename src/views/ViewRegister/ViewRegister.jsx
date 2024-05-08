38d71146f133ff06d66b78c649d9e924267461dd
RDG-Sanchez <rafaelzhx@gmail.com>
2024-05-07 23:30:10 -0300

    HOTFIX Update Register, Login, Profile & Products

--- src/views/ViewRegister/ViewRegister.jsx
+++ src/views/ViewRegister/ViewRegister.jsx
@@ -1,7 +1,21 @@
 // Import Components
 import { Navbar, Footer, Register } from "../../components/components";
 
+// Import Hooks
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
 const ViewRegister = () => {
+  const navigate = useNavigate();
+  useEffect(() => {
+    const localUser = JSON.parse(localStorage.getItem("user"));
+    if (localUser.token) {
+      navigate("/");
+    } else {
+      null;
+    }
+  }, []);
+
   return (
     <>
       <Navbar />
