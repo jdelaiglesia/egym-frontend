73445193cfa12b99261d95021004c94b7c4a7f0d
Dafting <joaquindlaiglesia@gmail.com>
2024-05-09 01:43:33 -0300

    featured con imagenes responsive, footer fixeado

---
+++ src/components/Featured/Images.jsx
@@ -0,0 +1,35 @@
+export const darkImage1 = new Image();
+darkImage1.src = "./slides/bannerDark1.svg";
+export const darkImage2 = new Image();
+darkImage2.src = "./slides/bannerDark2.svg";
+export const darkImage3 = new Image();
+darkImage3.src = "./slides/bannerDark3.svg";
+export const darkImage4 = new Image();
+darkImage4.src = "./slides/bannerDark4.svg";
+
+export const lightImage1 = new Image();
+lightImage1.src = "./slides/bannerLight1.svg";
+export const lightImage2 = new Image();
+lightImage2.src = "./slides/bannerLight2.svg";
+export const lightImage3 = new Image();
+lightImage3.src = "./slides/bannerLight3.svg";
+export const lightImage4 = new Image();
+lightImage4.src = "./slides/bannerLight4.svg";
+
+export const mdDarkImage1 = new Image();
+mdDarkImage1.src = "./slides/mdbannerDark1.svg";
+export const mdDarkImage2 = new Image();
+mdDarkImage2.src = "./slides/mdbannerDark2.svg";
+export const mdDarkImage3 = new Image();
+mdDarkImage3.src = "./slides/mdbannerDark3.svg";
+export const mdDarkImage4 = new Image();
+mdDarkImage4.src = "./slides/mdbannerDark4.svg";
+
+export const mdLightImage1 = new Image();
+mdLightImage1.src = "./slides/mdbannerLight1.svg";
+export const mdLightImage2 = new Image();
+mdLightImage2.src = "./slides/mdbannerLight2.svg";
+export const mdLightImage3 = new Image();
+mdLightImage3.src = "./slides/mdbannerLight3.svg";
+export const mdLightImage4 = new Image();
+mdLightImage4.src = "./slides/mdbannerLight4.svg";
