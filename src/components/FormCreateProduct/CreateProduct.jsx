5c0cb7aeb177b9fd2ce00114b2d13b9c5bcec845
Dafting <joaquindlaiglesia@gmail.com>
2024-05-10 16:13:24 -0300

    hotfix merge 2

--- src/components/FormCreateProduct/CreateProduct.jsx
+++ src/components/FormCreateProduct/CreateProduct.jsx
@@ -1,117 +1,169 @@
-import React, { useState } from "react";
-import axios from "axios";
-import validation from "./validation";
+import { useState, useEffect } from "react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import axios from "../../helpers/axios";
+import { useNavigate } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const CreateProduct = () => {
-  const [productData, setProductData] = useState({
-    name: "",
-    price: "",
-    available: "",
+  const [categories, setCategories] = useState([]);
+
+  const navigate = useNavigate();
+
+  const validationSchema = Yup.object({
+    name: Yup.string().required("Ingrese un nombre"),
+    stock: Yup.number().required("Ingrese un número de stock"),
+    price: Yup.number().required("Ingrese un precio"),
+    url_image: Yup.mixed().required("Ingrese una imagen"),
+    available: Yup.boolean().required("Seleccione una disponibilidad"),
+    category: Yup.string().required("Seleccione una categoría"),
+    description: Yup.string().required("Agrega una descripción"),
   });
-  const [errors, setErrors] = useState("");
 
-  const handleChange = (event) => {
-    const { name, value, type, checked } = event.target;
-    const newValue = type === "checkbox" ? checked : value;
-    setProductData((prevState) => ({
-      ...prevState,
-      [name]: newValue,
-    }));
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      stock: "",
+      available: "",
+      price: "",
+      url_image: null,
+      category: "",
+      description: "",
+    },
+    validationSchema,
+    onSubmit: async (values) => {
+      createProduct(values);
+    },
+  });
 
-    const validationErrors = validation({ ...productData, [name]: value });
-    setErrors(validationErrors);
-  };
-
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    try {
-      await axios.post(import.meta.env.VITE_BACKEND_URL, productData);
-    } catch (error) {
-      throw error;
-    }
-  };
-
-  const disableButton = () => {
-    const fieldEmpty =
-      !productData.name || !productData.price || !productData.available;
-
-    const hasErrors = Object.keys(errors).some((key) => errors[key]);
-    return fieldEmpty || hasErrors;
-  };
+  useEffect(() => {
+    axios("/categories").then(({ data }) => setCategories(data));
+  }, []);
 
   return (
-    <div>
-      <form
-        className="bg-slate-200 w-80 mx-auto mt-32 rounded-lg p-4 h-96"
-        onSubmit={handleSubmit}
-      >
-        <label className="input input-bordered flex items-center gap-2 mb-12">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 16 16"
-            fill="currentColor"
-            className="w-1 h-4 opacity-70"
-          ></svg>
+    <div className="w-96">
+      <form className="flex flex-col my-10 p-10" onSubmit={formik.handleSubmit}>
+        <h2 className="text-4xl font-bold text-center mb-6">
+          Publicar producto
+        </h2>
+        <div className="flex flex-col gap-2 mb-4">
           <input
             type="text"
-            onChange={handleChange}
-            value={productData.name}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.name}
             name="name"
-            className="grow"
-            placeholder="Name"
+            className="input input-bordered w-full max-w-xs"
+            placeholder="Nombre del producto"
           />
-        </label>
-        {errors.name && (
-          <p className="text-red-500 text-xs mb-8 -mt-10 absolute">
-            {errors.name}
-          </p>
-        )}
-        <label className="input input-bordered flex items-center gap-2 mb-5">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 16 16"
-            fill="currentColor"
-            className="w-1 h-4 opacity-70"
-          ></svg>
+
+          <span className="text-red-500 text-xs">
+            {formik.touched.name ? formik.errors.name : null}
+          </span>
+        </div>
+        <div className="flex flex-col gap-2 mb-4">
+          <textarea
+            type="text"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.description}
+            name="description"
+            className="textarea textarea-bordered"
+            placeholder="Descripción del producto"
+          />
+
+          <span className="text-red-500 text-xs">
+            {formik.touched.description ? formik.errors.description : null}
+          </span>
+        </div>
+        <div className="flex flex-col gap-2 mb-4">
           <input
             type="text"
-            onChange={handleChange}
-            value={productData.price}
-            name="price"
-            className="grow"
-            placeholder="Price"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.stock}
+            name="stock"
+            className="input input-bordered w-full max-w-xs"
+            placeholder="Número de stock"
           />
-        </label>
-        {errors.price && (
-          <p className="text-red-500 text-xs mb-5 -mt-2 absolute">
-            {errors.price}
-          </p>
-        )}
-        <label className="input input-bordered flex items-center gap-2 mb-5 mt-12">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 16 16"
-            fill="currentColor"
-            className="w-1 h-4 opacity-70"
-          ></svg>
-          <input
-            type="checkbox"
-            onChange={handleChange}
-            checked={productData.available}
-            name="available"
-            className="form-checkbox h-5 w-5 text-blue-500"
-          />
-          <span className="ml-2">Available</span>
-        </label>
 
-        <button
-          className="bg-blue-700 btn btn-xs sm:btn-sm md:btn-md lg:btn-lg w-full mt-4 text-white"
-          type="submit"
-          disabled={disableButton()}
-        >
-          Create
+          <span className="text-red-500 text-xs">
+            {formik.touched.stock ? formik.errors.stock : null}
+          </span>
+        </div>
+
+        <div className="flex flex-col gap-2 mb-4">
+          <select
+            className="select select-bordered text-base w-full max-w-xs"
+            name="available"
+            onBlur={formik.handleBlur}
+            onChange={formik.handleChange}
+            value={formik.values.available}
+          >
+            <option>Disponibilidad</option>
+            <option value="true">Disponible</option>
+            <option value="false">No disponible</option>
+          </select>
+
+          <span className="text-red-500 text-xs">
+            {formik.touched.available ? formik.errors.available : null}
+          </span>
+        </div>
+        <div className="flex flex-col gap-2 mb-4">
+          <input
+            type="text"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.price}
+            name="price"
+            className="input input-bordered w-full max-w-xs"
+            placeholder="Precio"
+          />
+
+          <span className="text-red-500 text-xs">
+            {formik.touched.price ? formik.errors.price : null}
+          </span>
+        </div>
+        <div className="flex flex-col gap-2 mb-4">
+          <input
+            type="file"
+            onChange={(event) => {
+              formik.setFieldValue("url_image", event.currentTarget.files[0]);
+            }}
+            onBlur={formik.handleBlur}
+            name="url_image"
+            className="file-input file-input-bordered w-full max-w-xs"
+            placeholder="URL de la imágen"
+          />
+
+          <span className="text-red-500 text-xs">
+            {formik.touched.url_image ? formik.errors.url_image : null}
+          </span>
+        </div>
+        <div className="flex flex-col gap-2 mb-4">
+          <select
+            className="select select-bordered text-base w-full max-w-xs"
+            name="category"
+            onBlur={formik.handleBlur}
+            onChange={formik.handleChange}
+            value={formik.values.category}
+          >
+            <option>Categoría</option>
+            {categories.map((category) => (
+              <option value={category._id}>{category.name}</option>
+            ))}
+          </select>
+
+          <span className="text-red-500 text-xs">
+            {formik.touched.category ? formik.errors.category : null}
+          </span>
+        </div>
+        <button className="btn btn-primary" type="submit">
+          Publicar
         </button>
       </form>
+      <ToastContainer />
     </div>
   );
 };
