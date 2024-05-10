47afc03f4ea8a3fdb987c13951d9f5c1a86dc9bf
RDG-Sanchez <rafaelzhx@gmail.com>
2024-05-10 20:42:41 -0300

    HOTFIX Axios URL

--- src/components/Featured/Featured.jsx
+++ src/components/Featured/Featured.jsx
@@ -6,21 +6,17 @@ import { ThemeContext } from "../../context/theme";
 import {
   darkImage1,
   darkImage2,
-  darkImage3,
   darkImage4,
   mdDarkImage1,
   mdDarkImage2,
-  mdDarkImage3,
   mdDarkImage4,
 } from "./Images";
 import {
   lightImage1,
   lightImage2,
-  lightImage3,
   lightImage4,
   mdLightImage1,
   mdLightImage2,
-  mdLightImage3,
   mdLightImage4,
 } from "./Images";
 
@@ -66,27 +62,6 @@ const Featured = () => {
             ❮
           </a>
           <a href="#slide3" className="btn btn-circle">
-            ❯
-          </a>
-        </div>
-      </div>
-      <div
-        id="slide3"
-        className="carousel-item relative w-full flex justify-center items-center bg-cover min-h-screen"
-      >
-        <img
-          src={theme === "black" ? darkImage3.src : lightImage3.src}
-          className="hidden md:block w-full"
-        />
-        <img
-          src={theme === "black" ? mdDarkImage3.src : mdLightImage3.src}
-          className="md:hidden w-full"
-        />
-        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <a href="#slide2" className="btn btn-circle">
-            ❮
-          </a>
-          <a href="#slide4" className="btn btn-circle">
             ❯
           </a>
         </div>
